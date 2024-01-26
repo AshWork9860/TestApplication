ef021f4d46cc3a820e39901c15d62930f3da7e71
AshWork9860 <157802273+AshWork9860@users.noreply.github.com>
2024-01-26 17:28:33 +0530

    Add files via upload

---
+++ TestApplicationWebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestApplication.DTO.Model;
+using TestApplication.Services.Interfaces;
+
+namespace TestApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var employees = _employeeService.GetAllEmployees();
+            return Ok(employees);
+        }
+
+
+        /// <summary>
+        /// this method can help to get Employee/Superwiser and Manager separetely by its employeeType(Manager/Superwiser/Employee)
+        /// </summary>
+        /// <param name="employeeType"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult GetAllEmployeesByType(int employeeType)
+        {
+            var employees = _employeeService.GetAllEmployees();
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(Employee employee)
+        {
+            _employeeService.AddEmployee(employee);
+            return Ok();
+        }
+    }
+}
